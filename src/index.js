26cea7a01aaa489e6a237198009eaeccde552ffa
dpp <2823901437@qq.com>
2020-07-29 23:33:39 +0800

    json

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import jsonData from './newpneumonia.json'
 
+console.log(jsonData)
 let root = document.getElementById('root')
 
-// function ListItem(props) {
-//   return (
-//     <li>
-//       <h3>title --- {props.item.title}</h3>
-//       <div>content ---- {props.item.content}</div>
-//     </li>
-//   )
-// }
 
-class ListItem extends React.Component {
+
+class List extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
@@ -20,53 +15,7 @@ class ListItem extends React.Component {
 
   render() {
     return (
-      <li
-        onClick={this.onClickHandler.bind(
-          this,
-          this.props.index,
-          this.props.item.title,
-        )
-      }>
-        <h3>title --- {this.props.item.title}</h3>
-        <div>content ---- {this.props.item.content}</div>
-      </li>
-    )
-  }
-
-  onClickHandler = (i, title, e) => {
-    alert(i + 1 + '-' + title)
-  }
-}
-
-class List extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      list: [
-        {
-          title: 'first',
-          content: 'content1'
-        },
-        {
-          title: 'second',
-          content: 'content2'
-        },
-        {
-          title: 'third',
-          content: 'content3'
-        }
-      ]
-    }
-  }
-
-  render() {
-    return (
       <div>
-        <ul>
-          {this.state.list.map((item, i) => {
-            return <ListItem key={i} index={i} item={item} />
-          })}
-        </ul>
       </div>
     )
   }
